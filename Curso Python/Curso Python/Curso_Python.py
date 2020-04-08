ca64d4b7840338073593c2dda58e16511d58ee97
Raul <raul.sierro@gmail.com>
2020-04-09 01:05:53 +0200

    01:05 09/04

---
+++ Curso Python/Curso Python/Curso_Python.py
@@ -0,0 +1,21 @@
+def suma (n1, n2, n3):
+
+    print(n1 + n2 - n3)
+
+suma(5, 3, 8)
+
+def operacion (num1, num2, num3):
+
+    resul = num1 +num2 * (num3 - num2)
+
+    return resul
+
+print(operacion(1, 2, 3))
+
+
+
+
+
+
+
+
