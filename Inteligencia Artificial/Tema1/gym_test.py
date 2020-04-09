087fe890ee0a3b8eb05456c00ded7cf818833efe
Raul <raul.sierro@gmail.com>
2020-04-09 02:10:16 +0200

    02:10 08/04

---
+++ Inteligencia Artificial/Tema1/gym_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+import gym
+
+environment = gym.make("MountainCar-v0)
+environment.rest()
+for _ in range(2000):
+    envaironment.step(envaironment.action_space.sample())
+    
