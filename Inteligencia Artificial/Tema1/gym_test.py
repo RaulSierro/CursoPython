4e83e6a4ec47fc5df63c9b9fe675b2c7b1694830
Raul <raul.sierro@gmail.com>
2020-04-09 02:30:17 +0200

    02:30 08/04

--- Inteligencia Artificial/Tema1/gym_test.py
+++ Inteligencia Artificial/Tema1/gym_test.py
@@ -6,8 +6,9 @@ Este es un archivo temporal.
 """
 import gym
 
-environment = gym.make("MountainCar-v0)
+environment = gym.make("MountainCar-v0")
 environment.rest()
 for _ in range(2000):
+    environment.render()
     envaironment.step(envaironment.action_space.sample())
     
